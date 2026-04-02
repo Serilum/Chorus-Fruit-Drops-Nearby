2dfb94c08627252caeaf10b6fb0707526b14954c
Rick South <rick@natamus.com>
2026-04-02 16:18:23 +0200

    Chorus Fruit Drops Nearby source code for 26.1.1.

---
+++ Common/src/main/java/com/natamus/chorusfruitdropsnearby/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.chorusfruitdropsnearby.util;
+
+public class Reference {
+	public static final String MOD_ID = "chorusfruitdropsnearby";
+	public static final String NAME = "Chorus Fruit Drops Nearby";
+	public static final String VERSION = "1.1";
+	public static final String ACCEPTED_VERSIONS = "[26.1.1]";
+}
